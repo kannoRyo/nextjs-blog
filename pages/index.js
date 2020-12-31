9b0ab2e77099af803a6f4d889462c950b299033e
kanno <kannoryo@icloud.com>
2020-12-31 10:01:21 +0900

    chapter-2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
@@ -9,9 +11,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -55,7 +60,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          <img src="/img/logo.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
 
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,19 @@
-const FirstPost= ()=> {
-    return <h1>First Post</h1>
-}
+import Link from 'next/link'
+import Head from 'next/head'
 
-export default FirstPost
-  
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+          <title>First Posts</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
